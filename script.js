4862d2c66f67384923dc99303f9516ace81cddaa
fernandovmedina <correosfernandomedina@gmail.com>
2024-07-03 16:57:19 -0600

    changes applied

--- script.js
+++ script.js
@@ -2,64 +2,60 @@ const pc1 = document.getElementById("pc_1");
 const pc2 = document.getElementById("pc_2");
 const pc3 = document.getElementById("pc_3");
 const pc4 = document.getElementById("pc_4");
-const pc5 = document.getElementById("pc_5");
+
+const pcc1 = document.getElementById("pcc-1");
+const pcc2 = document.getElementById("pcc-2");
+const pcc3 = document.getElementById("pcc-3");
+const pcc4 = document.getElementById("pcc-4");
 
 pc1.addEventListener('click', () => {
-    pc3.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc1.style.color = "green"
-    pc2.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc4.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc5.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    document.getElementById('titulo').innerHTML = "COMPRA DE PLASTICO"
-    document.getElementById('subtitulo').innerHTML = "¿QUÉ PRODUCTOS COMPRAMOS?"
-    document.getElementById("p_1").innerHTML = "Scrap plástico post industriales"
-    document.getElementById("p_2").innerHTML = "Compramos scrap plástico como, tarimas, cajas, contenedores, charolas, totes y diversos materiales."
+    pc1.style.color = 'green';
+    pc2.style.color = 'blue';
+    pc3.style.color = 'blue';
+    pc4.style.color = 'blue';
+
+    pcc1.style.display = 'block';
+    pcc2.style.display = 'none';
+    pcc3.style.display = 'none';
+    pcc4.style.display = 'none';
+    pcc5.style.display = 'none';
 })
 
 pc2.addEventListener('click', () => {
-    pc1.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc2.style.color = "green"
-    pc3.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc4.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc5.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    document.getElementById('titulo').innerHTML = "RECOLECCION DE PLASTICO"
-    document.getElementById('subtitulo').innerHTML = "Trabaja en colaboración con el cliente desarrollado productos según sus necesidades"
-    document.getElementById("p_1").innerHTML = ""
-    document.getElementById("p_2").innerHTML = ""
+    pc1.style.color = 'blue';
+    pc2.style.color = 'green';
+    pc3.style.color = 'blue';
+    pc4.style.color = 'blue';
+
+    pcc1.style.display = 'none';
+    pcc2.style.display = 'block';
+    pcc3.style.display = 'none';
+    pcc4.style.display = 'none';
+    pcc5.style.display = 'none';
 })
 
 pc3.addEventListener('click', () => {
-    pc1.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc3.style.color = "green"
-    pc2.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc4.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc5.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    document.getElementById('titulo').innerHTML = "LAVADO Y REPARACION"
-    document.getElementById('subtitulo').innerHTML = "Lavado y reparacion de equipo vacio (contenedores, tarimas, charolas termo formada y caja industrial)"
-    document.getElementById("p_1").innerHTML = ""
-    document.getElementById("p_2").innerHTML = ""
+    pc1.style.color = 'blue';
+    pc2.style.color = 'blue';
+    pc3.style.color = 'green';
+    pc4.style.color = 'blue';
+
+    pcc1.style.display = 'none';
+    pcc2.style.display = 'none';
+    pcc3.style.display = 'block';
+    pcc4.style.display = 'none';
+    pcc5.style.display = 'none';
 })
 
 pc4.addEventListener('click', () => {
-    pc1.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc4.style.color = "green"
-    pc2.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc3.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc5.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    document.getElementById('titulo').innerHTML = "FABRICACION DE PIEZAS"
-    document.getElementById('subtitulo').innerHTML = "Fabricacion de piezas especiales de grado automotriz"
-    document.getElementById("p_1").innerHTML = ""
-    document.getElementById("p_2").innerHTML = ""
-})
+    pc1.style.color = 'blue';
+    pc2.style.color = 'blue';
+    pc3.style.color = 'blue';
+    pc4.style.color = 'green';
 
-pc5.addEventListener('click', () => {
-    pc1.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc5.style.color = "green"
-    pc2.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc4.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    pc3.style.color = "rgb(30 58 138 / var(--tw-text-opacity))"
-    document.getElementById('titulo').innerHTML = "MAQUILADOS"
-    document.getElementById('subtitulo').innerHTML = "Maquilados de plasticos reciclabes. (Trituracion y molienda)"
-    document.getElementById("p_1").innerHTML = ""
-    document.getElementById("p_2").innerHTML = ""
+    pcc1.style.display = 'none';
+    pcc2.style.display = 'none';
+    pcc3.style.display = 'none';
+    pcc4.style.display = 'block';
+    pcc5.style.display = 'none';
 })
